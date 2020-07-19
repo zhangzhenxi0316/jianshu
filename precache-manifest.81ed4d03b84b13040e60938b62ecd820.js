5a95e3eae8aaa6485d36f075cf832f67497ba231
user <zhangzhenxi0316@163.com>
2020-07-19 23:30:55 +0800

    jianshu

---
+++ precache-manifest.81ed4d03b84b13040e60938b62ecd820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5d2c69f189bfa551980fabb624d9c2",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "782205b242f59ee06c36",
+    "url": "/jianshu/static/js/2.5e133ca6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jianshu/static/js/2.5e133ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8517d1edf51190071d2d",
+    "url": "/jianshu/static/js/3.960fa374.chunk.js"
+  },
+  {
+    "revision": "428291d91b3278f9253b",
+    "url": "/jianshu/static/js/main.dc39c32b.chunk.js"
+  },
+  {
+    "revision": "af12b571267efaf066df",
+    "url": "/jianshu/static/js/runtime-main.b60fe978.js"
+  }
+]);
